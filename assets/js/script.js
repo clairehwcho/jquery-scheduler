15238c86989f4d1fa9fde78e9700d7d74f612ddd
Claire <claire.hw.cho@gmail.com>
2022-12-12 18:46:52 -0800

    edit handleSaveEvent

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,54 +20,63 @@ $(function () {
 
   // The getEventsFromStorage function and return array of event objects.
   function getEventsFromStorage () {
-    let storedEventsArr = []
     const eventsFromStorage = localStorage.getItem("events");
     if (eventsFromStorage) {
       storedEventsArr = JSON.parse(eventsFromStorage);
+      console.log("Data found from local storage");
+      return storedEventsArr;
     }
-    return storedEventsArr;
+    else {
+      console.log("No data found from local storage");
+      return null;
+    }
   };
 
   // The storeEventsToStorage function saves an array of events in local storage.
   function storeEventsToStorage (storedEventsArr) {
     localStorage.setItem("events", JSON.stringify(storedEventsArr));
+    console.log("Data stored in local storage");
   };
 
   // Get event data from local storage and display it on the page.
   function renderEventData () {
-    // Clear all textareas on the page.
-    textareaEl.empty();
-
     // Get an array of events from local storage.
     const eventsArr = getEventsFromStorage();
 
-    // If any event object is stored in the array, add events to the corresponding timeblock.
-    if (eventsArr.length !== 0) {
-      // Loop through the array of events.
-      $(eventsArr).each(function (i) {
-        const event = eventsArr[i];
-        const eventHour = Object.keys(event)[0].substring(5);
-        const eventDescription = Object.values(event)[0];
-        // Loop through the timeblocks and add event description to the corresponding timeblock.
-        $(timeblockEl).each(function (j) {
-          const timeblock = $(this);
-          const timeblockHour = timeblock.attr("id").substring(5);
-          if (timeblockHour === eventHour) {
-            timeblock.children("textarea").text(eventDescription);
-          }
-        })
-      })
+    // If there is no data stored in local storage, exit the function.
+    if (!eventsArr) {
+      console.log("No data to be rendered");
+      return;
     }
+
+    // Clear all textareas on the page.
+    textareaEl.empty();
+
+    // If any event object is stored in the array, add events to the corresponding timeblock.
+    // Loop through the array of events.
+    $(eventsArr).each(function (i) {
+      const event = eventsArr[i];
+      const eventHour = Object.keys(event)[0].substring(5);
+      const eventDescription = Object.values(event)[0];
+      // Loop through the timeblocks and add event description to the corresponding timeblock.
+      $(timeblockEl).each(function () {
+        const timeblock = $(this);
+        const timeblockHour = timeblock.attr("id").substring(5);
+        if (timeblockHour === eventHour) {
+          timeblock.children("textarea").text(eventDescription);
+        }
+      })
+    })
+    console.log("Data rendered");
   };
 
   // The handleSaveEvent function stores event data in local storage.
   function handleSaveEvent (e) {
     e.preventDefault();
 
-    const hour = $(this).parent(".time-block").attr("id");
-    const description = $(this).siblings(".description").val();
-
     // Create an object for new event.
+    const hour = $(this).parent(".time-block").attr("id");
+    const description = $(this).siblings(".description").val().trim();
     const newEvent = {
       [hour]: description
     }
@@ -75,19 +84,21 @@ $(function () {
     // Get the array of events from local storage.
     let eventsArr = getEventsFromStorage();
 
-    // Add the new event object to the array.
-    // If the event array saved in local storage is empty
-    if (eventsArr.length === 0) {
-      if (description !== "") {
-        eventsArr.push(newEvent);
-        console.log("A new event added");
-      }
-      else {
+    // If no data is stored in local storage
+    if (eventsArr === null) {
+      // If the textarea is empty, exit the function.
+      if (description === "") {
         console.log("Nothing to update");
         return;
       }
+      // If the textarea is not empty, make a new array and add the new event object to the array.
+      else {
+        eventsArr = [];
+        eventsArr.push(newEvent);
+        console.log("A new event added");
+      }
     }
-    // If the event array saved in local storage is not empty
+    // If any data is stored in local storage
     else {
       let isPresent = false;
       while (!isPresent) {
@@ -98,30 +109,56 @@ $(function () {
           if (storedEventObj.hasOwnProperty(hour)) {
             if (storedEventObj[hour] === description) {
               console.log("Nothing to update");
-              return;
+              isPresent = true;
+              return false;
             }
             else if (description !== "") {
               storedEventObj[hour] = description;
               console.log(`${hour} data updated`);
               isPresent = true;
+              return false;
             }
             else {
-              eventsArr.splice($.inArray(storedEventObj[hour], eventsArr), 1);
+              // If the array has more than 1 element, find the corresponding index of the element
+              if (eventsArr.length > 1) {
+                let idxToRemove;
+                $.each(eventsArr, function (i, obj) {
+                  if (obj.hasOwnProperty(hour)) {
+                    idxToRemove = i;
+                  }
+                });
+                eventsArr.splice(idxToRemove, 1);
+                console.log("localstorage splice test");
+              }
+              else {
+                localStorage.removeItem("events");
+                console.log("localstorage delete test");
+                console.log(eventsArr);
+              }
               console.log(`${hour} data removed`);
               isPresent = true;
+              return false;
             }
           }
         })
+        break;
       }
-      // If the array does not have the selected hour data, add the new object to the array.
+
+      // If the array does not have the selected hour data, add nothing or the new object to the array.
       if (!isPresent) {
-        eventsArr.push(newEvent);
+        if (description === "") {
+          console.log("Nothing to update");
+          return;
+        }
+        else {
+          eventsArr.push(newEvent);
+          console.log("A new event added");
+        }
       }
     }
 
     // Save the array in local storage.
     storeEventsToStorage(eventsArr);
-    console.log("Data updated in local storage")
     saveMessageEl.insertAfter($("header"));
   };
 
